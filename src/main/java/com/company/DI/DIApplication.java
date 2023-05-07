fb87e557b60308453c7830e2a1ef61b3a54fa8fa
MahammadNiyazli <mahammad.niyazli@gmail.com>
2023-05-07 09:55:15 +0400

    prototype scope

--- src/main/java/com/company/DI/DIApplication.java
+++ src/main/java/com/company/DI/DIApplication.java
@@ -1,13 +1,25 @@
 package com.company.DI;
 
+import com.company.DI.prototype.Customer;
+import com.company.DI.prototype.CustomerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DIApplication.class, args);
+		ApplicationContext context = new AnnotationConfigApplicationContext(CustomerConfiguration.class);
+
+		Customer customer = context.getBean(Customer.class);
+		System.err.println(customer);
+
+		Customer customer2 = context.getBean(Customer.class);
+		System.err.println(customer2);
+
+		//SpringApplication.run(DIApplication.class, args);
 	}
 
 }
