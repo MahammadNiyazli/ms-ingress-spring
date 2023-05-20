c8a1fa582889fdd7866c25670fc4f130d09b107c
MahammadNiyazli <mahammad.niyazli@gmail.com>
2023-05-20 09:46:00 +0400

    lombok and slf4j used

--- src/main/java/com/company/DI/DIApplication.java
+++ src/main/java/com/company/DI/DIApplication.java
@@ -1,13 +1,29 @@
 package com.company.DI;
 
+import com.company.DI.dto.User;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DIApplication {
+public class DIApplication implements CommandLineRunner {
+
+	public DIApplication(Logger logger) {
+		this.logger = logger;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DIApplication.class, args);
 	}
 
+	private final Logger logger;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User(1L,"Mahammad","Niyazli",23);
+		logger.info(user.toString());
+		logger.error(user.toString());
+		logger.warn(user.toString());
+	}
 }
