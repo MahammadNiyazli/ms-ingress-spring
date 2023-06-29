cfc52f6e59174053feae4b6fa49e10a29980d862
MahammadNiyazli <mahammad.niyazli@gmail.com>
2023-06-29 21:40:29 +0400

    liquibase example

---
+++ src/main/java/com/company/DI/dao/entity/EmployeeEntity.java
@@ -0,0 +1,20 @@
+package com.company.DI.dao.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Integer age;
+}
